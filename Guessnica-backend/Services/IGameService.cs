af10ca6cc49fd909c66cf96d3cb80ead510b77cd
DenysZachyniaiev <denys.zachyniaiev@studenci.collegiumwitelona.pl>
2025-12-17 21:35:37 +0100

    handle riddle answer

--- Guessnica-backend/Services/IGameService.cs
+++ Guessnica-backend/Services/IGameService.cs
@@ -5,4 +5,5 @@ using Models;
 public interface IGameService
 {
     Task<UserRiddle> GetDailyRiddleAsync(string userId, int dailyHourUtc=0);
+    Task<UserRiddle> SubmitDailyAnswerAsync(string userId, decimal latitude, decimal longitude);
 }
